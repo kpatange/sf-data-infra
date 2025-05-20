8a857586fd8e0d3167e29f6fe57bd20b95bfda8c
kpatange <kpatange@gmail.com>
2025-05-20 10:11:40 +0000

    Added

--- snowsink/main.tf
+++ snowsink/main.tf
@@ -40,62 +40,62 @@ variable "pipe_create_flag" {
 }
 
 # Optional COPY statement tuning variables
-variable "on_error" {
+variable "copy_on_error" {
   type    = string
   default = "CONTINUE"
 }
 
-variable "size_limit" {
+variable "copy_size_limit" {
   type    = number
   default = null
 }
 
-variable "purge" {
+variable "copy_purge" {
   type    = bool
   default = null
 }
 
-variable "return_failed_only" {
+variable "copy_return_failed_only" {
   type    = bool
   default = null
 }
 
-variable "match_by_column_name" {
+variable "copy_match_by_column_name" {
   type    = string
   default = ""
 }
 
-variable "include_metadata" {
+variable "copy_include_metadata" {
   type    = string
   default = ""
 }
 
-variable "enforce_length" {
+variable "copy_enforce_length" {
   type    = bool
   default = null
 }
 
-variable "truncatecolumns" {
+variable "copy_truncatecolumns" {
   type    = bool
   default = null
 }
 
-variable "force" {
+variable "copy_force" {
   type    = bool
   default = null
 }
 
-variable "load_uncertain_files" {
+variable "copy_load_uncertain_files" {
   type    = bool
   default = null
 }
 
-variable "file_processor" {
+variable "copy_file_processor" {
   type    = string
   default = ""
 }
 
-variable "load_mode" {
+variable "copy_load_mode" {
   type    = string
   default = ""
 }
@@ -115,157 +115,157 @@ resource "snowflake_storage_integration" "integration" {
 
 
 # Optional Parameters
-variable "compression" {
+variable "file_format_compression" {
   type    = string
   default = null
 }
 
-variable "record_delimiter" {
+variable "file_format_record_delimiter" {
   type    = string
   default = null
 }
 
-variable "field_delimiter" {
+variable "file_format_field_delimiter" {
   type    = string
   default = null
 }
 
-variable "file_extension" {
+variable "file_format_file_extension" {
   type    = string
   default = null
 }
 
-variable "skip_header" {
+variable "file_format_skip_header" {
   type    = number
   default = null
 }
 
-variable "skip_blank_lines" {
+variable "file_format_skip_blank_lines" {
   type    = bool
   default = null
 }
 
-variable "date_format" {
+variable "file_format_date_format" {
   type    = string
   default = null
 }
 
-variable "time_format" {
+variable "file_format_time_format" {
   type    = string
   default = null
 }
 
-variable "timestamp_format" {
+variable "file_format_timestamp_format" {
   type    = string
   default = null
 }
 
-variable "binary_format" {
+variable "file_format_binary_format" {
   type    = string
   default = null
 }
 
-variable "escape" {
+variable "file_format_escape" {
   type    = string
   default = null
 }
 
-variable "escape_unenclosed_field" {
+variable "file_format_escape_unenclosed_field" {
   type    = string
   default = null
 }
 
-variable "null_if" {
+variable "file_format_null_if" {
   type    = list(string)
   default = null
 }
 
-variable "empty_field_as_null" {
+variable "file_format_empty_field_as_null" {
   type    = bool
   default = null
 }
 
-variable "field_optionally_enclosed_by" {
+variable "file_format_field_optionally_enclosed_by" {
   type    = string
   default = null
 }
 
-variable "trim_space" {
+variable "file_format_trim_space" {
   type    = bool
   default = null
 }
 
-variable "error_on_column_count_mismatch" {
+variable "file_format_error_on_column_count_mismatch" {
   type    = bool
   default = null
 }
 
-variable "replace_invalid_characters" {
+variable "file_format_replace_invalid_characters" {
   type    = bool
   default = null
 }
 
-variable "validate_utf8" {
+variable "file_format_validate_utf8" {
   type    = bool
   default = null
 }
 
-variable "skip_byte_order_mark" {
+variable "file_format_skip_byte_order_mark" {
   type    = bool
   default = null
 }
 
-variable "encoding" {
+variable "file_format_encoding" {
   type    = string
   default = null
 }
 
-variable "allow_duplicate" {
+variable "file_format_allow_duplicate" {
   type    = bool
   default = null
 }
 
-variable "strip_outer_array" {
+variable "file_format_strip_outer_array" {
   type    = bool
   default = null
 }
 
-variable "strip_null_values" {
+variable "file_format_strip_null_values" {
   type    = bool
   default = null
 }
 
-variable "ignore_utf8_errors" {
+variable "file_format_ignore_utf8_errors" {
   type    = bool
   default = null
 }
 
-variable "preserve_space" {
+variable "file_format_preserve_space" {
   type    = bool
   default = null
 }
 
-variable "column_names_in_first_line" {
+variable "file_format_column_names_in_first_line" {
   type    = bool
   default = null
 }
 
-variable "input_null_values" {
+variable "file_format_input_null_values" {
   type    = list(string)
   default = null
 }
 
-variable "output_null_values" {
+variable "file_format_output_null_values" {
   type    = list(string)
   default = null
 }
 
-variable "enable_octal" {
+variable "file_format_enable_octal" {
   type    = bool
   default = null
 }
 
-variable "disable_snowflake_identifier" {
+variable "file_format_disable_snowflake_identifier" {
   type    = bool
   default = null
 }
@@ -278,32 +278,32 @@ resource "snowflake_file_format" "file_format" {
   schema      = var.schema_name
   format_type = var.file_format_type
   ## Optional parameters for the file format
-  compression                      = var.compression
-  record_delimiter                 = var.record_delimiter
-  field_delimiter                  = var.field_delimiter
-  file_extension                   = var.file_extension
-  skip_header                      = var.skip_header
-  skip_blank_lines                 = var.skip_blank_lines
-  date_format                      = var.date_format
-  time_format                      = var.time_format
-  timestamp_format                 = var.timestamp_format
-  binary_format                    = var.binary_format
-  escape                           = var.escape
-  escape_unenclosed_field          = var.escape_unenclosed_field
-  null_if                          = var.null_if
-  empty_field_as_null              = var.empty_field_as_null
-  field_optionally_enclosed_by    = var.field_optionally_enclosed_by
-  trim_space                       = var.trim_space
-  error_on_column_count_mismatch  = var.error_on_column_count_mismatch
-  replace_invalid_characters      = var.replace_invalid_characters
-  skip_byte_order_mark             = var.skip_byte_order_mark
-  encoding                         = var.encoding
-  allow_duplicate                  = var.allow_duplicate
-  strip_outer_array                = var.strip_outer_array
-  strip_null_values                = var.strip_null_values
-  ignore_utf8_errors               = var.ignore_utf8_errors
-  preserve_space                   = var.preserve_space
-  enable_octal                     = var.enable_octal
+  compression                      = var.file_format_compression
+  record_delimiter                 = var.file_format_record_delimiter
+  field_delimiter                  = var.file_format_field_delimiter
+  file_extension                   = var.file_format_file_extension
+  skip_header                      = var.file_format_skip_header
+  skip_blank_lines                 = var.file_format_skip_blank_lines
+  date_format                      = var.file_format_date_format
+  time_format                      = var.file_format_time_format
+  timestamp_format                 = var.file_format_timestamp_format
+  binary_format                    = var.file_format_binary_format
+  escape                           = var.file_format_escape
+  escape_unenclosed_field          = var.file_format_escape_unenclosed_field
+  null_if                          = var.file_format_null_if
+  empty_field_as_null              = var.file_format_empty_field_as_null
+  field_optionally_enclosed_by    = var.file_format_field_optionally_enclosed_by
+  trim_space                       = var.file_format_trim_space
+  error_on_column_count_mismatch  = var.file_format_error_on_column_count_mismatch
+  replace_invalid_characters      = var.file_format_replace_invalid_characters
+  skip_byte_order_mark             = var.file_format_skip_byte_order_mark
+  encoding                         = var.file_format_encoding
+  allow_duplicate                  = var.file_format_allow_duplicate
+  strip_outer_array                = var.file_format_strip_outer_array
+  strip_null_values                = var.file_format_strip_null_values
+  ignore_utf8_errors               = var.file_format_ignore_utf8_errors
+  preserve_space                   = var.file_format_preserve_space
+  enable_octal                     = var.file_format_enable_octal
 }
 
 
@@ -340,18 +340,18 @@ resource "snowflake_pipe" "pipe" {
     COPY INTO ${var.database_name}.${var.schema_name}.${var.table_name}
     FROM @${var.database_name}.${var.schema_name}."${snowflake_stage.stage.name}"
     FILE_FORMAT = (FORMAT_NAME = ${var.database_name}.${var.schema_name}."${snowflake_file_format.file_format.name}")
-    ${var.on_error != "" ? "ON_ERROR = ${var.on_error}" : ""}
-    ${var.size_limit != null ? "SIZE_LIMIT = ${var.size_limit}" : ""}
-    ${var.purge != null ? "PURGE = ${var.purge}" : ""}
-    ${var.return_failed_only != null ? "RETURN_FAILED_ONLY = ${var.return_failed_only}" : ""}
-    ${var.match_by_column_name != "" ? "MATCH_BY_COLUMN_NAME = ${var.match_by_column_name}" : ""}
-    ${var.include_metadata != "" ? "INCLUDE_METADATA = (${var.include_metadata})" : ""}
-    ${var.enforce_length != null ? "ENFORCE_LENGTH = ${var.enforce_length}" : ""}
-    ${var.truncatecolumns != null ? "TRUNCATECOLUMNS = ${var.truncatecolumns}" : ""}
-    ${var.force != null ? "FORCE = ${var.force}" : ""}
-    ${var.load_uncertain_files != null ? "LOAD_UNCERTAIN_FILES = ${var.load_uncertain_files}" : ""}
-    ${var.file_processor != "" ? "FILE_PROCESSOR = (${var.file_processor})" : ""}
-    ${var.load_mode != "" ? "LOAD_MODE = ${var.load_mode}" : ""}
+    ${var.copy_on_error != "" ? "ON_ERROR = ${var.on_error}" : ""}
+    ${var.copy_size_limit != null ? "SIZE_LIMIT = ${var.size_limit}" : ""}
+    ${var.copy_purge != null ? "PURGE = ${var.purge}" : ""}
+    ${var.copy_return_failed_only != null ? "RETURN_FAILED_ONLY = ${var.return_failed_only}" : ""}
+    ${var.copy_match_by_column_name != "" ? "MATCH_BY_COLUMN_NAME = ${var.match_by_column_name}" : ""}
+    ${var.copy_include_metadata != "" ? "INCLUDE_METADATA = (${var.include_metadata})" : ""}
+    ${var.copy_enforce_length != null ? "ENFORCE_LENGTH = ${var.enforce_length}" : ""}
+    ${var.copy_truncatecolumns != null ? "TRUNCATECOLUMNS = ${var.truncatecolumns}" : ""}
+    ${var.copy_force != null ? "FORCE = ${var.force}" : ""}
+    ${var.copy_load_uncertain_files != null ? "LOAD_UNCERTAIN_FILES = ${var.load_uncertain_files}" : ""}
+    ${var.copy_file_processor != "" ? "FILE_PROCESSOR = (${var.file_processor})" : ""}
+    ${var.copy_load_mode != "" ? "LOAD_MODE = ${var.load_mode}" : ""}
   EOT
 
 
