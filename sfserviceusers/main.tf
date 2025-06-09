123ce69eec7d46bf84bde79cf76d2d61314028fe
kpatange <kpatange@gmail.com>
2025-06-09 20:25:50 +0000

    Added

--- sfserviceusers/main.tf
+++ sfserviceusers/main.tf
@@ -314,7 +314,7 @@ resource "tls_private_key" "snowflake_key" {
 resource "azurerm_key_vault_secret" "private_key" {
   name           = var.private_key_name
   value          = tls_private_key.snowflake_key.private_key_pem_pkcs8(local.actual_passphrase)
-  key_vault_id   = local.key_vault_id # This line should start on a new line after the 'value' line
+  key_vault_id   = local.key_vault_id # This *must* be on a new line
 
   depends_on = [
     azurerm_private_endpoint.kv_private_endpoint,
